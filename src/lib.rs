9d83d2a31dcf71c6430c28b6eaf9d02009767c92
Josh <josh@joshthomas.dev>
2024-10-13 18:25:16 -0500

    add stub compile function and basic integration tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,13 @@ mod error;
 mod lexer;
 mod scanner;
 mod token;
+
+use std::error::Error;
+
+pub fn compile(_template: &str) -> Result<String, Box<dyn Error>> {
+    todo!("Implement compilation process")
+}
+
+// re-export important types or functions from modules here, e.g.
+// pub use lexer::Lexer;
+// that should be part of the public API
