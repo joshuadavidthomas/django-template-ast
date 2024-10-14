c0cd3ba89e3db6a91384225df9f1f57c1ee9243f
Josh <josh@joshthomas.dev>
2024-10-14 14:50:38 -0500

    add error constructor methods to LexerError

--- src/error.rs
+++ src/error.rs
@@ -4,14 +4,35 @@ use std::fmt;
 pub enum LexerError {
     EmptyToken(usize),
     UnexpectedCharacter(char, usize),
+    LexicalError { message: String, position: usize },
+}
+
+impl LexerError {
+    pub fn empty_token<T>(line: usize) -> Result<T, Self> {
+        Err(LexerError::EmptyToken(line))
+    }
+
+    pub fn unexpected_character<T>(character: char, line: usize) -> Result<T, Self> {
+        Err(LexerError::UnexpectedCharacter(character, line))
+    }
+
+    pub fn lexical_error<T>(message: &str, position: usize) -> Result<T, Self> {
+        Err(LexerError::LexicalError {
+            message: message.to_string(),
+            position,
+        })
+    }
 }
 
 impl fmt::Display for LexerError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             LexerError::EmptyToken(line) => write!(f, "Empty token at line {}", line),
             LexerError::UnexpectedCharacter(c, line) => {
                 write!(f, "Unexpected character '{}' at line {}", c, line)
+            }
+            LexerError::LexicalError { message, position } => {
+                write!(f, "Lexical error at position {}: {}", position, message)
             }
         }
     }
